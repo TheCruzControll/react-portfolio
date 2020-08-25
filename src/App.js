903420dcb4337c4d35db3dd28babcd260b5ede0a
Niko Cruz <francisnikocruz@gmail.com>
2020-08-25 00:32:47 -0700

    added under construction screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hey!</h1>
+      <h3>My website is currently under construction. </h3>
+      <h3>
+        If you wanna talk, feel free to email me at{" "}
+        <a href="mailto:francisnikocruz@gmail.com">francisnikocruz@gmail.com</a>
+      </h3>
     </div>
   );
 }
